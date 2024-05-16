37650d3af1be8bbacae898fb5416aa95fbef8db0
jed709 <jedidiah709@gmail.com>
2024-05-16 00:11:36 -0230

    Updated data and scripts - all working

--- scripts/2.1.6 Quinlan and Taylor 2019 E4.r
+++ scripts/2.1.6 Quinlan and Taylor 2019 E4.r
@@ -9,15 +9,6 @@ qt_e7_studydat %>%
 #d' calculations 
 qt_e7_test_sum %>%
   select(participant, condition, is_old, mean) %>%
-  mutate(instruction = paste0(condition, is_old)) %>%
-  select(-condition, -is_old) %>%
-  pivot_wider(id_cols = participant, names_from = 'instruction', values_from = 'mean') %>%
-  mutate(read_d = calc_d_between(read1, read0, cor = .004), 
-         sing_d = calc_d_between(sing1, sing0, cor = .004), 
-         speak_d = calc_d_between(speak1, speak0, cor = .004)) %>%
-  select(participant, read_d:speak_d) %>%
-  pivot_longer(cols = c(read_d:speak_d), names_to = 'condition', values_to = 'd') %>%
-  drop_na(d) -> qt_e7_d_sum
-
-#d cor
-cor(qt_e7_d_sum[qt_e7_d_sum$condition=='sing_d',]$d, qt_e7_d_sum[qt_e7_d_sum$condition=='speak_d',]$d)
+  pivot_wider(id_cols = c(participant, condition), names_from = 'is_old', values_from = 'mean') %>%
+  mutate(d = calc_d(`1`, `0`, cor = .004)) %>%
+  select(participant, condition, d) -> qt_e7_d_sum
